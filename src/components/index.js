07a8be599749c840f29c58ad1706788ccdbc1eec
mh-saeed <saeedkhan111244@gmail.com>
2021-08-19 23:25:16 +0500

    Completed

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./routs/Home";
+export { default as About } from "./routs/About";
+export { default as Products } from "./routs/Products";
+export { default as ProductItem } from "./routs/ProductItem";
+export { default as Navigation } from "./navbar/Navigation";
