db0ce87d8fa81283acd0741587622f026161cf36
mh-saeed <saeedkhan111244@gmail.com>
2021-08-19 23:25:03 +0500

    shoes data added

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const shoes = {
+  "air-jordan-3-valor-blue": {
+    name: "VALOUR BLUE",
+    img: "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "jordan-mars-270-london": {
+    name: "JORDAN 270 LONDON",
+    img: "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "air-jordan-1-zoom-racer-blue": {
+    name: "RACER BLUE",
+    img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "air-jordan-1-zoom-racer-blue-pro": {
+    name: "RACER BLUE PREMIUM",
+    img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "air-jordan-2-zoom-racer-blue": {
+    name: "RACER BLUE PREMIUM",
+    img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "jordan-mars-277-london": {
+    name: "JORDAN 270 PAK",
+    img: "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "air-jordan-7-valor-blue": {
+    name: "VALOUR BLUE ASPIRE",
+    img: "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "air-jordan-8-zoom-racer-blue": {
+    name: "RACER BLUE BOLD",
+    img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+};
+export default shoes;
